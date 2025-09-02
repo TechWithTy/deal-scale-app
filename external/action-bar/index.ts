9008519eec540031d7d5a1f355e1e7c06b55e423
TechWithTy <tyrique1.daniel@gmail.com>
2025-09-01 21:21:43 -0600

    Remove existing external/action-bar before submodule conversion

--- external/action-bar/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./components/providers/CommandPaletteProvider";
-export { default as ActionBarRoot } from "./components/ActionBarRoot";
-export { StandaloneCommandPaletteProvider } from "./components/providers/StandaloneCommandPaletteProvider";
-export * as Web from "./web";
