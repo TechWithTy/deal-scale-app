8a614db181460b4d3ab9d96b1ed822fd36d573f7
TechWithTy <tyrique1.daniel@gmail.com>
2025-09-11 21:13:49 -0600

    Remove existing external/activity-graph before submodule conversion

--- external/activity-graph/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ActivityLineGraph } from "./ActivityLineGraph";
-export { default as CustomNode } from "./CustomNode";
-export { default as CustomPopover } from "./CustomPopover";
-export { default as ActivityLineGraphContainer } from "./ActivityLineGraphContainer";
-export { default as TeamActivityFeed } from "./TeamActivityFeed";
